1faa2edfe097d4eb1f1ae6ce2aabbb80439faed7
tuotuo <909709223@qq.com>
2018-05-09 19:05:50 +0800

    springboot资源文件属性配置

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,5 +1,9 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.pojo.IMoocJSONResult;
+import com.example.springboot.pojo.Resource;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private Resource resource;
+
     @RequestMapping("/hello")
     private String index(){
         return "Hello World!";
+    }
+
+    @RequestMapping("/getResource")
+    public IMoocJSONResult getResource(){
+        Resource bean = new Resource();
+        BeanUtils.copyProperties(resource, bean);
+        return IMoocJSONResult.ok(bean);
     }
 }
