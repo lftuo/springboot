1df472ef831d67295d54ac4e6a60d98ab8d30682
tuotuo <909709223@qq.com>
2018-05-10 21:40:41 +0800

    thymeleaf标签&SpringBoot配置全局的异常捕获方式

--- src/main/java/com/example/springboot/controller/ThymeleafController.java
+++ src/main/java/com/example/springboot/controller/ThymeleafController.java
@@ -1,8 +1,13 @@
 package com.example.springboot.controller;
 
+import com.example.springboot.pojo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("th")
@@ -19,4 +24,38 @@ public class ThymeleafController {
         return "thymeleaf/center/center";
     }
 
+
+    @RequestMapping("test")
+    public String test(ModelMap map) {
+
+        User u = new User();
+        u.setName("superadmin");
+        u.setAge(10);
+        u.setPassword("123465");
+        u.setBirthday(new Date());
+        u.setDesc("<font color='green'><b>hello imooc</b></font>");
+
+        map.addAttribute("user", u);
+
+        User u1 = new User();
+        u1.setAge(19);
+        u1.setName("imooc");
+        u1.setPassword("123456");
+        u1.setBirthday(new Date());
+
+        User u2 = new User();
+        u2.setAge(17);
+        u2.setName("LeeCX");
+        u2.setPassword("123456");
+        u2.setBirthday(new Date());
+
+        List<User> userList = new ArrayList<>();
+        userList.add(u);
+        userList.add(u1);
+        userList.add(u2);
+
+        map.addAttribute("userList", userList);
+
+        return "thymeleaf/test";
+    }
 }
---
+++ src/main/java/com/example/springboot/exception/SpringBootExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.springboot.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+public class SpringBootExceptionHandler {
+
+    public static final String SPRING_BOOT_VIEW = "error";
+
+    @ExceptionHandler(value = Exception.class)
+    public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
+        e.printStackTrace();
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("url", request.getRequestURL());
+        mav.setViewName(SPRING_BOOT_VIEW);
+
+        return mav;
+    }
+
+}
