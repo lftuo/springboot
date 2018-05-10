c8df105f5aa7f3133e3017f81e07c2837e033237
tuotuo <909709223@qq.com>
2018-05-11 07:05:48 +0800

    SpringBoot ajax/web统一异常捕获方式：判断是否为ajax请求

--- src/main/java/com/example/springboot/exception/SpringBootAjaxExceptionHandler.java
+++ src/main/java/com/example/springboot/exception/SpringBootAjaxExceptionHandler.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class SpringBootAjaxExceptionHandler {
 
-    @ExceptionHandler(value = Exception.class)
+    //@ExceptionHandler(value = Exception.class)
     public IMoocJSONResult defaultErrorHandler(HttpServletRequest request, Exception e) throws Exception{
         e.printStackTrace();
         return IMoocJSONResult.errorException(e.getMessage());
--- src/main/java/com/example/springboot/exception/SpringBootExceptionHandler.java
+++ src/main/java/com/example/springboot/exception/SpringBootExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.springboot.exception;
 
+import com.example.springboot.pojo.IMoocJSONResult;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,13 +8,13 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@ControllerAdvice
+@ControllerAdvice
 public class SpringBootExceptionHandler {
 
     public static final String SPRING_BOOT_VIEW = "error";
 
     //@ExceptionHandler(value = Exception.class)
-    public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
+    /*public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws Exception{
         e.printStackTrace();
         ModelAndView mav = new ModelAndView();
         mav.addObject("exception", e);
@@ -21,6 +22,33 @@ public class SpringBootExceptionHandler {
         mav.setViewName(SPRING_BOOT_VIEW);
 
         return mav;
+    }*/
+
+    @ExceptionHandler(value = Exception.class)
+    public Object errorHandler(HttpServletRequest request, HttpServletResponse response, Exception e){
+        e.printStackTrace();
+
+        if (isAjax(request)){
+            return IMoocJSONResult.errorException(e.getMessage());
+        } else {
+
+            ModelAndView mav = new ModelAndView();
+            mav.addObject("exception", e);
+            mav.addObject("url", request.getRequestURL());
+            mav.setViewName(SPRING_BOOT_VIEW);
+
+            return mav;
+        }
+    }
+
+    /**
+     * 判断是否是ajax请求
+     * @param request
+     * @return
+     */
+    public static boolean isAjax(HttpServletRequest request){
+        return (request.getHeader("X-Requested-with") != null
+                && "XMLHttpRequest".equals(request.getHeader("X-Requested-with").toString()));
     }
 
 }
