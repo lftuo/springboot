d69acd139b13da892ffd4313e1c7b362ae4eb978
tuotuo <909709223@qq.com>
2018-05-13 16:11:37 +0800

    springboot定时任务表达式的简单讲解与应用

--- src/main/java/com/example/springboot/task/TestTask.java
+++ src/main/java/com/example/springboot/task/TestTask.java
@@ -11,7 +11,8 @@ public class TestTask {
     private static final SimpleDateFormat dataFormat = new SimpleDateFormat("HH:mm:ss");
 
     // 定义每过3秒执行
-    @Scheduled(fixedRate = 3000)
+    // @Scheduled(fixedRate = 3000)
+    @Scheduled(cron = "0/4 * * * * ? ")
     public void reportCurrentTime(){
         System.out.println("现在时间：" + dataFormat.format(new Date()));
     }
